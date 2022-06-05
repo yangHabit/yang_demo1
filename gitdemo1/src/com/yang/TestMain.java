5e4811cc6f0aa2c9bf1583f3a05eb67f91534747
${yang} <${2280539387@qq.com}>
2022-06-05 15:37:36 +0800

    尝试第二次更改

--- gitdemo1/src/com/yang/TestMain.java
+++ gitdemo1/src/com/yang/TestMain.java
@@ -8,5 +8,7 @@ package com.yang;
  * @fuction: about the role of class.
  */
 public class TestMain {
-
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
 }
