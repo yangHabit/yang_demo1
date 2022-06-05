6d025bc88283fb2be94e0b7e5889c29ee87789ec
${yang} <${2280539387@qq.com}>
2022-06-05 15:29:42 +0800

    尝试第一次更改

---
+++ gitdemo1/src/com/yang/TestMain.java
@@ -0,0 +1,12 @@
+package com.yang;
+
+/**
+ * @author: YangJiao
+ * @email 1320259466@qq.com
+ * @date: 2022/6/5
+ * @time: 15:26
+ * @fuction: about the role of class.
+ */
+public class TestMain {
+
+}
